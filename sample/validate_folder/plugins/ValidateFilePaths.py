f9051cf7778dbdabba08fe3898629d88efc2bcb6
hannes.delbeke <hannesdelbeke@gmail.com>
2022-01-29 19:55:31 +0000

    add doc

--- sample/validate_folder/plugins/ValidateFilePaths.py
+++ sample/validate_folder/plugins/ValidateFilePaths.py
@@ -2,6 +2,11 @@ import pyblish.api
 import pathlib2 as pathlib
 
 class ValidateFilePaths(pyblish.api.InstancePlugin):
+    """
+    Check all paths follow the correct format
+    Attributes:
+        pattern_name (str): The pattern to be used.
+    """
     label = "check FileNames starts with"
     order = pyblish.api.ValidatorOrder
     families = ['paths']
